e38587da85f53f9c081fe1f232c19b845d56b70b
Iain Lee <icleen@gmail.com>
2019-05-16 15:38:32 -0600

    setup to combine images based on the affine transform

--- affine_transform.py
+++ affine_transform.py
@@ -2,11 +2,13 @@ import os, sys
 import numpy as np
 from scipy import misc
 from scipy import ndimage
+from PIL import Image
+import matplotlib.pyplot as plt
 
 
 def read_points(file):
     with open(file, 'r') as f:
-        lines = [[int(val) for val in line.strip().split()]
+        lines = [[float(val) for val in line.strip().split()]
                                 for line in f if len(line) > 1]
     # print(file)
     # for line in lines:
@@ -19,7 +21,7 @@ def get_pts(bef, aft):
     pt_dimp = pt_dim+1
     pts = np.zeros((len(bef)*pt_dim, pt_dimp*2))
     pts_prime = np.zeros((len(bef)*pt_dim,))
-    for i in range(pt_dimp):
+    for i in range(len(bef)):
         j = i*pt_dim
         for ind in range(pt_dim):
             pts[j,ind] = bef[i][ind]
@@ -33,35 +35,62 @@ def get_pts(bef, aft):
 
 
 def get_affine(bef, aft):
-    pts, pts_prime = get_pts(bef[:3], aft[:3])
-    ptsi = np.linalg.inv(pts)
+    pts, pts_prime = get_pts(bef, aft)
+    ptsi = np.linalg.pinv(pts)
     affine = np.dot(ptsi, pts_prime)
 
-    chek = np.matmul(pts, affine)
-    assert np.sum(np.rint(chek) == np.rint(pts_prime)) == chek.shape[0]
+    # import pdb; pdb.set_trace()
+    # chek = np.matmul(pts, affine)
+    # assert np.sum(np.rint(chek) == np.rint(pts_prime)) == chek.shape[0]
+    #
+    # pts2, pts_prime2 = get_pts(bef[3:6], aft[3:6])
+    # ptsi2 = np.linalg.inv(pts2)
+    # affine2 = np.dot(ptsi2, pts_prime2)
+    # chek2 = np.matmul(pts2, affine2)
+    # assert np.sum(np.rint(chek2) == np.rint(pts_prime2)) == chek2.shape[0]
+    #
+    # chek3 = np.matmul(pts, affine2)
+    # chek4 = np.matmul(pts2, affine)
+    #
+    # print(chek)
+    # print(chek2)
+    # print(chek3)
+    # print(chek4)
+    # assert np.sum(np.rint(chek3) == np.rint(pts_prime2)) == chek3.shape[0]
+    #
+    # assert np.sum(affine == affine2) == affine.shape[0]
 
-    pts2, pts_prime2 = get_pts(bef[3:6], aft[3:6])
-    ptsi2 = np.linalg.inv(pts2)
-    affine2 = np.dot(ptsi2, pts_prime2)
-    chek2 = np.matmul(pts2, affine2)
-    assert np.sum(np.rint(chek2) == np.rint(pts_prime2)) == chek2.shape[0]
+    return affine
 
-    chek3 = np.matmul(pts, affine2)
-    chek4 = np.matmul(pts2, affine)
 
-    print(chek)
-    print(chek2)
-    print(chek3)
-    print(chek4)
-    assert np.sum(np.rint(chek3) == np.rint(pts_prime2)) == chek3.shape[0]
+def combine_imgs(img1, img2, affine):
+    img1 = Image.open(img1)
+    img2 = Image.open(img2)
+    nwidth = 900
+    nheight = 500
 
-    assert np.sum(affine == affine2) == affine.shape[0]
-    return a
+    imgt2 = img2.transform((nwidth, nheight),
+        Image.AFFINE, data=affine.flatten()[:6], resample=Image.NEAREST)
+    imgt2 = np.asarray(imgt2)
+
+    result = np.zeros((nheight, nwidth))
+
+    result += imgt2
+    imgt1 = np.asarray(img1)
+    shp = ((0, nheight-imgt1.shape[0]), (0, nwidth-imgt1.shape[1]))
+    imgt1 = np.pad(imgt1, shp,
+        'constant', constant_values=0)
+    result += imgt1
+
+    plt.imshow(result)
+    plt.show()
 
 
 def main():
     bef = "test_data/2D_registration/correspondence2D_w0.txt"
     aft = "test_data/2D_registration/correspondence2D_w1.txt"
+    # bef = "test_data/3D_registration/correspondence3D_0.txt"
+    # aft = "test_data/3D_registration/correspondence3D_1.txt"
     # bef = "taffine_test1.txt"
     # aft = "taffine_test2.txt"
     bef = read_points(bef)
@@ -71,7 +100,17 @@ def main():
     assert len(bef) >= pt_dim
 
     affine = get_affine(bef, aft)
-    print( affine )
+
+    img1 = 'test_data/2D_registration/w0.png'
+    img2 = 'test_data/2D_registration/w1.png'
+    combine_imgs(img1, img2, affine)
+
+    # imgt1 = img1.transform((img1.size[0], img1.size[1]),
+    #     Image.AFFINE, data=affine.flatten()[:6], resample=Image.NEAREST)
+    # plt.imshow(np.asarray(imgt1))
+    # plt.show()
+
+
 
     # pts = np.zeros((len(bef)*2, pt_dim*2))
     # pts_prime = np.zeros((len(bef)*(pt_dim-1),))
